1aa6e2c2c3ad10dd45ac57d98cf721b19c602fe8
Hugh Hoang <hugh.hoang@gmail.com>
2021-08-25 00:23:24 +1200

    refactor: game engine cpp (#2)
    * refactor: only .hpp for engine
    
    * refactor: dont need all pointers

--- packages/mitmeo/animated_sprite.hpp
+++ packages/mitmeo/animated_sprite.hpp
@@ -10,7 +10,7 @@ namespace mitmeo
         uint32_t _frame_time;
         uint32_t _last_time_ms;
         float _frame_rate;
-        std::vector<uint8_t> *_sprites;
+        std::vector<uint8_t> _sprites;
         uint8_t _sprite_index;
         uint8_t _sprite_count;
 
@@ -22,21 +22,16 @@ namespace mitmeo
             _frame_time = 0;
             _frame_rate = 1.0;
             _sprite_index = 0;
-            _sprites = new std::vector<uint8_t>(0);
         }
 
-        void set_sprites(std::vector<uint8_t> sprites)
+        void set_sprites(const std::vector<uint8_t> sprites)
         {
-            _sprites->clear();
-            for (auto sprite : sprites)
-            {
-                _sprites->push_back(sprite);
-            }
+            _sprites = sprites;
             _sprite_index = 0;
             _sprite_count = sprites.size();
         }
 
-        void set_fps(uint8_t fps)
+        void set_fps(const uint8_t fps)
         {
             _frame_rate = 1.0 / fps;
         }
@@ -56,7 +51,7 @@ namespace mitmeo
                     _frame_time = time_ms;
                 }
             }
-            return (*_sprites)[_sprite_index];
+            return _sprites[_sprite_index];
         }
     };
 }
--- packages/mitmeo/game_engine.hpp
+++ packages/mitmeo/game_engine.hpp
@@ -24,14 +24,54 @@ namespace mitmeo
             return *_instance;
         }
 
-        void internal_update(uint32_t time);
-        void internal_render(uint32_t time_ms);
-        void internal_add_entity(Entity *entity);
+        void internal_update(uint32_t time)
+        {
+            for (auto i : _entities)
+            {
+                i->update(time);
+            }
+        }
+        void internal_render(uint32_t time_ms)
+        {
+            for (auto i : _entities)
+            {
+                i->render(time_ms);
+            }
+        }
+        void internal_add_entity(Entity *entity)
+        {
+            _entities.push_back(entity);
+        }
 
     public:
-        static void update(uint32_t time);
+        static void update(uint32_t time_ms);
         static void render(uint32_t time_ms);
         static void add_entity(Entity *entity);
-        static void terminate();
     };
+
+    // Allocate the memory
+    GameEngine *GameEngine::_instance = nullptr;
+
+    void GameEngine::update(uint32_t time_ms)
+    {
+        get_instance().internal_update(time_ms);
+    }
+
+    void GameEngine::render(uint32_t time_ms)
+    {
+        get_instance().internal_render(time_ms);
+    }
+
+    void GameEngine::add_entity(Entity *entity)
+    {
+        get_instance().internal_add_entity(entity);
+    }
+
+    GameEngine::GameEngine() {}
+
+    GameEngine::~GameEngine()
+    {
+        delete _instance;
+        _instance = nullptr;
+    }
 }
