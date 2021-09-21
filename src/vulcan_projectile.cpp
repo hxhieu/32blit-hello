496ed4f42314acd8ea46019464348cda29ea7b9c
Hugh Hoang <hugh.hoang@gmail.com>
2021-09-22 00:20:22 +1200

    chore: cmake

--- src/vulcan_projectile.cpp
+++ src/vulcan_projectile.cpp
@@ -11,7 +11,7 @@ namespace mitmeo
             world.emplace<components::Sprite>(
                 entt,
                 std::vector<uint8_t>{sprites::vulcan_1, sprites::vulcan_2});
-            world.emplace<components::Velocity>(entt, dir_x, dir_y, 1);
+            world.emplace<components::Velocity>(entt, dir_x, dir_y, 3);
             // Center pivot
             world.emplace<components::Position>(entt, x, y);
             world.emplace<components::AutoDestroy>(entt);
